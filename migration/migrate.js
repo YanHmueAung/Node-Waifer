6ea2dd27c8ba7cfb5b83e725d1ef3a3b7db372e9
yan <yanhmueaung@gmail.com>
2021-10-28 00:16:54 +0800

    Json reading file

--- migration/migrate.js
+++ migration/migrate.js
@@ -3,8 +3,11 @@ let backup = async () => {
     let data = [{
         id: "lasdf", name: "aserwef"
     }]
-    await fs.writeFileSync("./migration/text.txt", JSON.stringify(data), 'utf8');
-
+    //await fs.writeFileSync("./migration/text.txt", JSON.stringify(data), 'utf8');
+    let result = await fs.readFileSync("./migration/text.txt", "utf8");
+    result = JSON.parse(result);
+    console.log(typeof result);
+    console.log(result);
 
 }
 
